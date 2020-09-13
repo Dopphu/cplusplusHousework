f09fb6467246287eecc62e6969e96378ae517306
Dopphu <2513429530@qq.com>
2020-09-13 12:44:35 +0800

    瞎改前版本[C

--- main.cpp
+++ main.cpp
@@ -12,24 +12,27 @@ void searchScore();
 void print();
 int main(){
 	cout<<"欢迎来到学生信息管理系统"<<endl;
-    Student demo1("Dopphu", "201978030304", 100, 99, 98);
-    Student demo2("Doyyli", "201978030314", 82, 79, 68);
-    Student demo3("大哥", "201978030324", 70, 90, 98);
+//    Student demo1("Dopphu", "201978030304", 100, 99, 98);
+//    Student demo2("Doyyli", "201978030314", 82, 79, 68);
+//    Student demo3("大哥", "201978030324", 70, 90, 98);
+    
 //    demo1.printAll();
 //	demo1.setScore("math", 66);
 //    demo1.printAll();
     cout<<"project running"<<endl;
     int times = 0;
     char selection;
-    demo.addStudent(demo1);
+//    demo.addStudent(demo1);
 //    demo.addStudent(demo2);
 //    demo.addStudent(demo3);
-//    demo.addStudent(demo3);
+//    demo.printAllStudent();
 	//和游戏一样，程序死循环开始！ 
 	while(true){
 //    		demo.addStudent(demo3);
+//			demo.addStudent(demo3);
+//   			demo.printAllStudent();
 			cout<<"Q/q 退出！"<<endl
-    		<<"P/p 打印所有学生成绩"
+    		<<"P/p 打印所有学生成绩"<<endl 
 			<<"A/a 录入学生信息"<<endl
 			<<"C/c 修改学生成绩"<<endl;
     		cin>>selection;
@@ -76,14 +79,14 @@ void add(){
 	char flag = 'n';
 	cin>>flag;
 	if(flag == 'y'){
-//		Student temp(n, num, c, m, e); 
-//		demo.addStudent(demo1);
-		demo.addStudent(demo3);
+		Student temp(num, n, c, m, e); 
+		temp.printAll();
+		demo.addStudent(temp);
 		/*缺少一个往学生表添加的功能,只是创建了没有往表里添加*/
 		cout<<"信息成功录入！"<<endl;
 	}else{
 		cout<<"信息未录入。"<<endl; 
-	} 
+	}
 }
 void searchScore(){
 	cout<<"这是查询功能"<<endl;
@@ -91,7 +94,7 @@ void searchScore(){
 void print(){
 	demo.printAllStudent();
 }
-/*这个修改功能有问题，待修复...*/
+/*这个修改功能有问题，待修复...me:问题可能出在链表的插入模块*/
 void changeScore(){
 	/*仍然需要表的使用，因为要通过姓名进行定位*/
 	cout<<"请输入修改科目及分数"<<endl;
